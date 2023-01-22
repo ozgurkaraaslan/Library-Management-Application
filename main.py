6f3eac703146b8e5391bdb1dc4207edb1405b2d7
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-01-23 02:05:48 +0300

    feat: MongoDB Implementation

--- main.py
+++ main.py
@@ -1,71 +1,106 @@
 import json
+import pymongo
 
-db = [{
-        "book": [
-            {
-                "title": "",
-                "id": None,
-                "author": "",
-                "subject_category": "",
-                "publication_date": "",
-                "physical_address": "",
-                "stock_quantity": None,
-                "book_state": ""
-            }
-        ]
-    },
-    {
-        "user": [
-            {
-                "username": "Ozgur",
-                "password": 456,
-                "occupied_books": [""]
-            }
-        ]
-    },
-    {
-        "librarian_password": 1234
-    }]
+username = "rebelkrone"
+password = "JqlXmO7xuMhkDAoM"
+
+client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.yah8b9u.mongodb.net/?retryWrites=true&w=majority")
+db = client["mydatabase"]
+books_col = db["books"]
+users_col = db["users"]
+librarian_col = db["librarian"]
 
 print("Welcome to the Library Management System.")
 print('''
-1. Librarian Login
-2. User Login
-3. User Sign Up''')
+1. User Operations
+2. Librarian Operations''')
 
-def librarian_login(passwd):
-    if passwd == db[2]["librarian_password"]:
-        print("Login is successfull.")
-    else:
-        print("Login is not successfull!")
+class User:
+    def __init__(self, users_col):
+            self.users_col = users_col
 
-def user_login(uname, passwd):
-    for i in db[1]["user"]:
-        if i["username"] == uname:
-            if i["password"] == passwd:
-                print("Login Succesfull.")
-            else:
-                print("Login is not successfull!")
-            return 
+    def user_login(self):
+        username = input("Username: ")
+        password = input("Password: ")
 
-    print("User not found!")   
+        if self.users_col.find_one({"$and": [{"username": username}, {"password": password}]}):
+            print("Login is succesfull.")
+            return 1
+        else:
+            print("Login is not successfull!")
+            return 0
+
+    def user_register(self):
+        username = input("Username: ")
+        password = input("Password: ")
+
+        self.users_col.insert_one({
+                    "username": username,
+                    "password": password,
+                    "occupied_books": []
+                })
+        print("Sign Up Successfull")
+    def user_operations(self):
+        print('''
+1. Show Books
+2. Show Users
+3. Librarian Logout''')
+        #librarian_action = int(input("Choose an option for continue: "))
+
+class Librarian:
+    def __init__(self, librarian_col):
+            self.librarian_col = librarian_col
+
+    def librarian_login(self):
+        librarian_password = input("Password: ")
+
+        if librarian_password == self.librarian_col.find_one()["librarian_password"]:
+            print("Login is successfull.")
+            return 1
+        else:
+            print("Login is not successfull!")
+            return 0
+
+    def librarian_operations(self):
+        print('''
+1. Show Books
+2. Show Users
+3. Librarian Logout''')
+        librarian_action = int(input("Choose an option for continue: "))
+
+        if librarian_action == 3:
+            print("Logged out.")
+            return 1
 
 while 1:
     input_type = int(input("Choose an option for continue: "))
     if input_type == 1:
-        librarian_password = int(input("Password: "))
-        librarian_login(librarian_password)
-        break
+        user = User(users_col)
+
+        print('''
+1. User Login
+2. User Sign Up''')
+        input_type = int(input("Choose an option for continue: "))
+
+        if input_type == 1:
+            if user.user_login():
+                user.user_operations()
+                break
+            else:
+                print("Try Again.")
+        elif input_type == 2:
+            user.user_register()       
+            
     elif input_type == 2:
-        username = input("Username: ")
-        password = int(input("Password: "))
-        user_login(username, password)
-        break
-    elif input_type == 3:
-        username = input("New Username: ")
-        password = input("New Password: ")
-        user_register(username, password)
-        break
-    else:
-        print("Invalid input! Try again.\n")
+        librarian = Librarian(librarian_col)
+
+        if librarian.librarian_login():
+            librarian.librarian_operations()
+        else:
+            print("Try Again.")
+
+    print("\nWelcome to the Library Management System.")    
+    print('''
+1. User Operations
+2. Librarian Operations''')
 
