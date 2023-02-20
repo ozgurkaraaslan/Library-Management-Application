fd58ffb76cb88790909b48b703498d87999d69eb
Ozgur Karaaslan <ozgurkaraaslan29@gmail.com>
2023-02-20 18:14:51 +0300

    feat: test directory was added.

---
+++ src/tests/test_book.py
@@ -0,0 +1,18 @@
+import pytest
+import main
+
+
+def test_search_by_title():
+    pass
+
+
+def test_search_by_author():
+    pass
+
+
+def test_search_by_subject_category():
+    pass
+
+
+def test_search_by_publication_date():
+    pass
